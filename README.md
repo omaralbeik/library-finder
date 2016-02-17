eae7386199917976f27711c765d5f99460bd8f9e
Omar Albeik <omaralbeik@gmail.com>
2016-02-17 12:34:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# library-finder
+#Library Finder
+
+### Getting started
+#### 1. Download the entire project zipped file from the repository:
+
+#### 2. install Node.js and npm if not installed in your device:
+https://docs.npmjs.com/getting-started/installing-node
+
+#### 3. move to project directory and install npm packages and required bower libraries:
+``` bash
+$> cd /project_path (change this to project directory)
+$> npm install
+$> bower install
+```
+#### 4. run project
+type gulp to run all tasks required to produce the code, and then type gulp serve to run the produced code:
+
+``` bash
+$> gulp
+$> gulp serve
+```
---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+var gulp = require('gulp');
+
+var mainBowerFiles = require('gulp-main-bower-files');
+var browserSync = require("browser-sync").create();
+var reload = browserSync.reload;
+var cssnano = require('gulp-cssnano');
+var replace = require("gulp-html-replace");
+var sourcemaps = require("gulp-sourcemaps");
+var imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+
+var dest = 'dist/';
+
+gulp.task('bower', function() {
+  return gulp.src('./bower.json')
+    .pipe(mainBowerFiles({
+      overrides: {
+        bootstrap: {
+          main: [
+            './dist/js/*.min.*',
+            './dist/css/*.min.*',
+            './dist/fonts/*.*'
+          ]
+        }
+      }
+    }))
+    .pipe(gulp.dest(dest + 'libs'));
+});
+
+gulp.task('html', function() {
+  gulp.src('./src/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: false
+    }))
+    .pipe(gulp.dest(dest))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('js', function() {
+  gulp.src('./src/**/*.js')
+    // comment the next line to disbale maping js files
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(jsmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    // comment the next line to disbale maping js files
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(dest))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('css', function() {
+  gulp.src('src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dest))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('img', function() {
+  gulp.src(['src/**/*.png', 'src/**/*.jpg', 'src/**/*.gif', 'src/**/*.jpeg'])
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(imageresize({
+      width: 960,
+      upscale: false
+    }))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+  gulp.watch('./src/**/*.html', ['html']);
+  gulp.watch('./src/**/*.js', ['scripts']);
+  gulp.watch('./src/**/*.css', ['styles']);
+  gulp.watch('bower_components/**', ['bower']);
+});
+
+gulp.task('default', ['js', 'css', 'html', 'bower']);
